1886fb8251d9a855b8972d909ea796cdcdf18b31
Leonardo Mariz Bezerra <leonardomariz@alu.ufc.br>
2026-07-19 19:36:33 -0300

    preenchendo categoria

--- src/NerdStore.Catalogo.Domain/Produto.cs
+++ src/NerdStore.Catalogo.Domain/Produto.cs
@@ -57,8 +57,23 @@ public class Produto : Entity, IAgregateRoot
     {
         return QuantidadeEstoque >= quantidadeEstoque;
     }
+
+    public void Validar()
+    {
+        
+    }
 }
 
 public class Categoria : Entity
 {
+    public string Nome { get; private set; }
+    public int Codigo { get; private set; }
+
+    public Categoria(string nome, int codigo)
+    {
+        Nome = nome;
+        Codigo = codigo;
+    }
+
+    public override string ToString() => $"{Nome} - {Codigo}";
 }
