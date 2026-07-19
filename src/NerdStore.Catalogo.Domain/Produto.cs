185e046ef70bf6562e8b9950d5fdae4bd42b19f7
Leonardo Mariz Bezerra <leonardomariz@alu.ufc.br>
2026-07-19 19:03:05 -0300

    populando propriedades na entidade Produto

--- src/NerdStore.Catalogo.Domain/Produto.cs
+++ src/NerdStore.Catalogo.Domain/Produto.cs
@@ -5,6 +5,13 @@ namespace NerdStore.Catalogo.Domain;
 // Interface of aggregation: Aggregate Root
 public class Produto : Entity, IAgregateRoot
 {
+    public string Nome { get; private set; }
+    public string Descricao { get; private set; }
+    public bool Ativo { get; private set; }
+    public decimal Valor { get; private set; }
+    public DateTime DataCadastro { get; private set; }
+    public string Imagem { get; private set; }
+    public int QuantidadeEstoque { get; private set; }
 }
 
 public class Categoria : Entity
