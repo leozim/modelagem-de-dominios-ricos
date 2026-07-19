68d6d967476cde9db4d3209cf9c7695ea5fbf734
Leonardo Mariz Bezerra <leonardomariz@alu.ufc.br>
2026-07-19 18:49:03 -0300

    criando Entity global e override no equals

---
+++ src/NerdStore.Catalogo.Domain/Produto.cs
@@ -0,0 +1,11 @@
+﻿using NerdStore.Core.DomainObjects;
+
+namespace NerdStore.Catalogo.Domain;
+
+public class Produto : Entity
+{
+}
+
+public class Categoria : Entity
+{
+}
---
+++ src/NerdStore.Core/DomainObjects/Entity.cs
@@ -0,0 +1,21 @@
+﻿namespace NerdStore.Core.DomainObjects;
+
+public abstract class Entity
+{
+    public Guid Id { get; set; }
+
+    protected Entity()
+    {
+        Id = Guid.NewGuid();
+    }
+
+    public override bool Equals(object obj)
+    {
+        var compareTo = obj as Entity;
+
+        if (ReferenceEquals(this, compareTo)) return true;
+        if(ReferenceEquals(null, compareTo)) return false;
+        
+        return Id.Equals(compareTo.Id);
+    }
+}
