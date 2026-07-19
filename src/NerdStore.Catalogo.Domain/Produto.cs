49c18dfe6a2229800c0a667312f8fbbf6f34bea3
Leonardo Mariz Bezerra <leonardomariz@alu.ufc.br>
2026-07-19 19:24:49 -0300

    criando ad-hoc setter e contrutor de produto

--- src/NerdStore.Catalogo.Domain/Produto.cs
+++ src/NerdStore.Catalogo.Domain/Produto.cs
@@ -5,6 +5,7 @@ namespace NerdStore.Catalogo.Domain;
 // Interface of aggregation: Aggregate Root
 public class Produto : Entity, IAgregateRoot
 {
+    public Guid CategoriaId { get; private set; }
     public string Nome { get; private set; }
     public string Descricao { get; private set; }
     public bool Ativo { get; private set; }
@@ -12,6 +13,28 @@ public class Produto : Entity, IAgregateRoot
     public DateTime DataCadastro { get; private set; }
     public string Imagem { get; private set; }
     public int QuantidadeEstoque { get; private set; }
+
+    public Categoria Categoria { get; private set; }
+
+    public Produto(Guid categoriaId,
+                   string nome, 
+                   string descricao, 
+                   bool ativo, 
+                   decimal valor, 
+                   DateTime dataCadastro, 
+                   string imagem)
+    {
+        CategoriaId = categoriaId;
+        Nome = nome;
+        Descricao = descricao;
+        Ativo = ativo;
+        Valor = valor;
+        DataCadastro = dataCadastro;
+        Imagem = imagem;
+    }
+
+    public void Ativar() => Ativo = true;
+    public void Desativar() => Ativo = false;
 }
 
 public class Categoria : Entity
