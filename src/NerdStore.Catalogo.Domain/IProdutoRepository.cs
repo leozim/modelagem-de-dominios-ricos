385037bbbee9c7b03de19428070c32311abb8f2d
Leonardo Mariz Bezerra <leonardomariz@alu.ufc.br>
2026-07-19 21:35:33 -0300

    usando unit of work

---
+++ src/NerdStore.Catalogo.Domain/IProdutoRepository.cs
@@ -0,0 +1,8 @@
+﻿using NerdStore.Core.Data;
+
+namespace NerdStore.Catalogo.Domain;
+
+public interface IProdutoRepository : IRepository<Produto>
+{
+    // parei em 4:21 no vídeo Persistindo com repositórios
+}
---
+++ src/NerdStore.Core/Data/IRepository.cs
@@ -0,0 +1,8 @@
+﻿using NerdStore.Core.DomainObjects;
+
+namespace NerdStore.Core.Data;
+
+public interface IRepository<T> : IDisposable where T : IAgregateRoot
+{
+    IUnitOfWork UnitOfWork { get; }
+}
