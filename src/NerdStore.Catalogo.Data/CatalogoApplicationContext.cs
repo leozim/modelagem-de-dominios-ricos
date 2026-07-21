8ce5fbd6a747c223dcb11c9be17fad05f99a4748
Leonardo Mariz Bezerra <leonardomariz@alu.ufc.br>
2026-07-20 22:41:11 -0300

    criando mapping de produto e categoria

--- src/NerdStore.Catalogo.Data/CatalogoApplicationContext.cs
+++ src/NerdStore.Catalogo.Data/CatalogoApplicationContext.cs
@@ -1,8 +1,30 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using NerdStore.Catalogo.Domain;
+using NerdStore.Core.Data;
 
 namespace NerdStore.Catalogo.Data;
 
-public class CatalogoApplicationContext : DbContext
+public class CatalogoApplicationContext : DbContext, IUnitOfWork
 {
+    public DbSet<Produto> Produtos { get; set; }
+    public DbSet<Categoria> Categorias { get; set; }
     
+    public CatalogoApplicationContext(DbContextOptions<CatalogoApplicationContext> options) 
+        : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        foreach (var property in modelBuilder.Model.GetEntityTypes().SelectMany(e => e.GetProperties()
+                     .Where(p => p.ClrType == typeof(string))))
+        {
+            property.SetColumnType("varchar(100)"); // evita nvarchar MAX
+        }
+        
+        base.OnModelCreating(modelBuilder);
+    }
+
+    public async Task<bool> Commit()
+    {
+        throw new NotImplementedException();
+    }
 }
