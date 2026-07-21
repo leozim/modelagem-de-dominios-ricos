ac390b5fcce8cc6cf9d098f824dd04fb000c5947
Leonardo Mariz Bezerra <leonardomariz@alu.ufc.br>
2026-07-20 23:33:33 -0300

    implementando serviço de estoque

---
+++ src/NerdStore.Catalogo.Domain/EstoqueServices.cs
@@ -0,0 +1,6 @@
+﻿namespace NerdStore.Catalogo.Domain;
+
+public class EstoqueServices
+{
+    
+}
---
+++ src/NerdStore.Catalogo.Domain/IEstoqueServico.cs
@@ -0,0 +1,7 @@
+﻿namespace NerdStore.Catalogo.Domain;
+
+public interface IEstoqueServico : IDisposable
+{
+    Task<bool> DebitarEstoque(Guid produtoId, int quantidade);
+    Task<bool> ReporEstoque(Guid produtoId, int quantidade);
+}
