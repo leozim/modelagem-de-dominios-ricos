e692a617a17cbe44469812a7e2cd5a279be99225
Leonardo Mariz Bezerra <leonardomariz@alu.ufc.br>
2026-07-22 16:05:15 -0300

    configurando automapper com value object para Produto

---
+++ src/NerdStore.Catalogo.Application/AutoMapper/DomainToDtoMappingProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using NerdStore.Catalogo.Application.DTOs;
+using NerdStore.Catalogo.Domain;
+
+namespace NerdStore.Catalogo.Application.AutoMapper;
+
+public class DomainToDtoMappingProfile : Profile
+{
+    public DomainToDtoMappingProfile()
+    {
+        CreateMap<Produto, ProdutoDto>()
+            .ForMember(d => d.Largura, o => o.MapFrom(s => s.Dimensoes.Largura))
+            .ForMember(d => d.Altura, o => o.MapFrom(s => s.Dimensoes.Altura))
+            .ForMember(d => d.Profundidade, o => o.MapFrom(s => s.Dimensoes.Profundidade))
+            .ReverseMap();
+        CreateMap<Categoria, CategoriaDto>().ReverseMap();
+    }
+}
