54f1e36f6a0a05f28a4c7b2276214a740da8c888
Leonardo Mariz Bezerra <leonardomariz@alu.ufc.br>
2026-07-22 15:40:57 -0300

    criando repositorio da camada de aplicacao para produto e categoria

---
+++ src/NerdStore.Catalogo.Application/Services/IProdutoAppService.cs
@@ -0,0 +1,18 @@
+﻿using NerdStore.Catalogo.Application.DTOs;
+
+namespace NerdStore.Catalogo.Application.Services;
+
+public interface IProdutoAppService : IDisposable
+{
+    Task<IEnumerable<ProdutoDto>> ObterPorCategoria(int codigo);
+    Task<ProdutoDto> ObterPorId(Guid id);
+    Task<IEnumerable<ProdutoDto>> ObterTodos();
+    Task<IEnumerable<CategoriaDto>> ObterCategorias();
+    
+    Task AdicionarProduto(ProdutoDto produto);
+    Task AtualizarProduto(ProdutoDto produto);
+    
+    Task<ProdutoDto> DebitarEstoque(Guid id, int quantidade);
+    Task<ProdutoDto> ReporEstoque(Guid id, int quantidade);
+
+}
