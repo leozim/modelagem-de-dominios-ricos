e17c3b9d0ad878153b4e7c446a8e8c18ec45658f
Leonardo Mariz Bezerra <leonardomariz@alu.ufc.br>
2026-07-25 16:39:39 -0300

    definindo vitrine como rota home

--- src/NerdStore.WebApp.MVC/Program.cs
+++ src/NerdStore.WebApp.MVC/Program.cs
@@ -34,7 +34,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Vitrine}/{action=Index}/{id?}");
 app.MapRazorPages();
 
 app.Run();
