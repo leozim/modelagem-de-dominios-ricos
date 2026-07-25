14c4bf61f9cc1375307e0afa4b9ee8cf24a98700
Leonardo Mariz Bezerra <leonardomariz@alu.ufc.br>
2026-07-25 17:01:06 -0300

    registrando dependencia do mediator

--- src/NerdStore.WebApp.MVC/Setup/DependencyInjection.cs
+++ src/NerdStore.WebApp.MVC/Setup/DependencyInjection.cs
@@ -1,7 +1,9 @@
-﻿using NerdStore.Catalogo.Application.Services;
+﻿using MediatR;
+using NerdStore.Catalogo.Application.Services;
 using NerdStore.Catalogo.Data;
 using NerdStore.Catalogo.Data.Repository;
 using NerdStore.Catalogo.Domain;
+using NerdStore.Catalogo.Domain.Events;
 using NerdStore.Core.Bus;
 
 namespace NerdStore.WebApp.MVC.Setup;
@@ -18,5 +20,7 @@ public static class DependencyInjection
         services.AddScoped<IProdutoAppService, ProdutoAppService>();
         services.AddScoped<IEstoqueService, EstoqueService>();
         // services.AddScoped<CatalogoApplicationContext>();
+
+        services.AddScoped<INotificationHandler<ProdutoAbaixoEstoqueEvent>, ProdutoEventHandler>();
     }
 }
