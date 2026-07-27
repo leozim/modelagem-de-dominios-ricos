a3979647d7acf1fb9149b993e4842674ef208de3
Leonardo Mariz Bezerra <leonardomariz@alu.ufc.br>
2026-07-27 19:10:26 -0300

    adicionando propriedades em pedidos, pedido item e voucher

--- src/NerdStore.Vendas.Domain/Pedido.cs
+++ src/NerdStore.Vendas.Domain/Pedido.cs
@@ -4,5 +4,39 @@ namespace NerdStore.Vendas.Domain;
 
 public class Pedido : Entity, IAgregateRoot
 {
+    private readonly List<PedidoItem> _pedidoItems;
     
+    public int Codigo { get; private set; }
+    public Guid ClientId { get; private set; }
+    public Guid? VoucherId { get; private set; }
+    public bool VoucherUtilizado { get; private set; }
+    public decimal Desconto { get; private set; }
+    public decimal ValorTotal { get; private set; }
+    public DateTime DataCadastro { get; private set; }
+    public PedidoStatus PedidoStatus { get; private set; }
+    
+    public IReadOnlyCollection<PedidoItem> PeidoItems => _pedidoItems.AsReadOnly();
+    
+    // EF Rel.
+    public Voucher Voucher { get; private set; }
+
+    protected Pedido() { }
+    public Pedido(int codigo, 
+                  Guid clientId, 
+                  Guid? voucherId, 
+                  bool voucherUtilizado, 
+                  decimal desconto, 
+                  decimal valorTotal, 
+                  DateTime dataCadastro, 
+                  PedidoStatus pedidoStatus)
+    {
+        Codigo = codigo;
+        ClientId = clientId;
+        VoucherId = voucherId;
+        VoucherUtilizado = voucherUtilizado;
+        Desconto = desconto;
+        ValorTotal = valorTotal;
+        DataCadastro = dataCadastro;
+        PedidoStatus = pedidoStatus;
+    }
 }
--- src/NerdStore.Vendas.Domain/PedidoItem.cs
+++ src/NerdStore.Vendas.Domain/PedidoItem.cs
@@ -4,5 +4,27 @@ namespace NerdStore.Vendas.Domain;
 
 public class PedidoItem : Entity
 {
-    
+    public Guid PedidoId { get; private set; }
+    public Guid ProdutoId { get; private set; }
+    public string ProdutoNome { get; private set; }
+    public int Quantidade { get; private set; }
+    public decimal ValorUnitario { get; private set; }
+
+    // EF Rel.
+    public Pedido Pedido { get; set; }
+
+    public PedidoItem(Guid pedidoId, 
+                      Guid produtoId, 
+                      string produtoNome, 
+                      int quantidade, 
+                      decimal valorUnitario, 
+                      Pedido pedido)
+    {
+        PedidoId = pedidoId;
+        ProdutoId = produtoId;
+        ProdutoNome = produtoNome;
+        Quantidade = quantidade;
+        ValorUnitario = valorUnitario;
+        Pedido = pedido;
+    }
 }
