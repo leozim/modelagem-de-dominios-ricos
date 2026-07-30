ba4f14b5727f4f33df8fe1ac91b12fd3ccefcf2d
Leonardo Mariz Bezerra <leonardomariz@alu.ufc.br>
2026-07-30 20:06:30 -0300

    corrigindo necessidade de dois DB

---
+++ src/NerdStore.Vendas.Data/VendasCOntextFactory.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace NerdStore.Vendas.Data;
+
+public class VendasCOntextFactory : IDesignTimeDbContextFactory<VendasContext>
+{
+    public VendasContext CreateDbContext(string[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<VendasContext>();
+
+        var connectionString =
+            "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=NerdStoreDb;Integrated Security=True;pooling=True";
+        
+        optionsBuilder.UseSqlServer(connectionString);
+        
+        return new VendasContext(optionsBuilder.Options);
+    }
+}
