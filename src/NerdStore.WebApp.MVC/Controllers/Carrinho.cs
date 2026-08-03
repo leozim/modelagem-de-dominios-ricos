5218d71513aa85aa8e092d8834e41f31e524cfc6
Leonardo Mariz Bezerra <leonardomariz@alu.ufc.br>
2026-08-03 18:39:45 -0300

    usando mediator com command de pedido

---
+++ src/NerdStore.WebApp.MVC/Controllers/Carrinho.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NerdStore.Catalogo.Application.Services;
+using NerdStore.Core.Bus;
+using NerdStore.Vendas.Application.Commands;
+
+namespace NerdStore.WebApp.MVC.Controllers;
+
+public class Carrinho : ControllerBase
+{
+    private readonly IProdutoAppService _produtoAppService;
+    private readonly IMediatorHandler _mediatorHandler;
+
+    public Carrinho(IProdutoAppService produtoAppService, 
+                    IMediatorHandler mediatorHandler)
+    {
+        _produtoAppService = produtoAppService;
+        _mediatorHandler = mediatorHandler;
+    }
+
+    // GET
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    [Route("meu-carrinho")]
+    public async Task<IActionResult> AdicionarItem(Guid id, int quantidade)
+    {
+        var produto = await _produtoAppService.ObterPorId(id);
+
+        if (produto == null) return BadRequest();
+
+        if (produto.QuantidadeEstoque < quantidade)
+        {
+            TempData["Erro"] = "Produto com estoque insuficiente";
+            return RedirectToAction("ProdutoDetalhe", "Vitrine", new { id });
+        }
+
+        var command = new AdicionarItemPedidoCommand(ClienteId, produto.Id, produto.Nome, quantidade, produto.Valor);
+        await _mediatorHandler.EnviarComando(command);
+
+        TempData["Erro"] = "Produto indisponível";
+        return RedirectToAction("ProdutoDetalhe", "Vitrine", new { id });
+    }
+}
