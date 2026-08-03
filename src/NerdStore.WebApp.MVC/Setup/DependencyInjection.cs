0584be26db2b6b289141a62a4e660a0c06c9059f
Leonardo Mariz Bezerra <leonardomariz@alu.ufc.br>
2026-08-03 19:28:01 -0300

    dependency injection

--- src/NerdStore.WebApp.MVC/Setup/DependencyInjection.cs
+++ src/NerdStore.WebApp.MVC/Setup/DependencyInjection.cs
@@ -5,6 +5,7 @@ using NerdStore.Catalogo.Data.Repository;
 using NerdStore.Catalogo.Domain;
 using NerdStore.Catalogo.Domain.Events;
 using NerdStore.Core.Communication.Mediator;
+using NerdStore.Core.Messages.CommonMessages.Notifications;
 using NerdStore.Vendas.Application.Commands;
 using NerdStore.Vendas.Data.Repository;
 using NerdStore.Vendas.Domain;
@@ -15,8 +16,11 @@ public static class DependencyInjection
 {
     public static void RegisterServices(this IServiceCollection services)
     {
-        // Domain Buss (Mediator)
+        // (Mediator)
         services.AddScoped<IMediatorHandler, MediatorHandler>();
+        
+        // Notifications
+        services.AddScoped<INotificationHandler<DomainNotification>, DomainNotificationHandler>();
         
         // Catalogo
         services.AddScoped<IProdutoRepository, ProdutoRepository>();
