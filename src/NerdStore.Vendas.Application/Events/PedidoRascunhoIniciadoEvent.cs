39474b6a08db8de9818c9a6955d119e273dfc4fc
Leonardo Mariz Bezerra <leonardomariz@alu.ufc.br>
2026-08-12 17:38:06 -0300

    criando evento para pedido em rascunho

---
+++ src/NerdStore.Vendas.Application/Events/PedidoRascunhoIniciadoEvent.cs
@@ -0,0 +1,15 @@
+﻿using NerdStore.Core.Messages;
+
+namespace NerdStore.Vendas.Application.Events;
+
+public class PedidoRascunhoIniciadoEvent : Event
+{
+    public Guid ClienteId { get; private set; }
+    public Guid PedidoId { get; private set; }
+
+    public PedidoRascunhoIniciadoEvent(Guid clienteId, Guid pedidoId)
+    {
+        ClienteId = clienteId;
+        PedidoId = pedidoId;
+    }
+}
