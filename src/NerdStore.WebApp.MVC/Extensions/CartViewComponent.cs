205145ce5d34563d8527f5792a3dcbd5b2887820
Leonardo Mariz Bezerra <leonardomariz@alu.ufc.br>
2026-08-12 20:26:57 -0300

    view component do carrinho

---
+++ src/NerdStore.WebApp.MVC/Extensions/CartViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NerdStore.Vendas.Application.Queries;
+
+namespace NerdStore.WebApp.MVC.Extensions;
+
+public class CartViewComponent : ViewComponent
+{
+    private readonly IPedidoQueries _pedidoQueries;
+
+    protected Guid ClienteId = Guid.Parse("244f74bf-158e-4778-b058-8372df7107a9");
+
+    public CartViewComponent(IPedidoQueries pedidoQueries)
+    {
+        _pedidoQueries = pedidoQueries;
+    }
+
+    public async Task<IViewComponentResult> InvokeAsync()
+    {
+        var carrinho = await _pedidoQueries.ObterCarrinhoCliente(ClienteId);
+        var itens = carrinho?.Items.Count ?? 0;
+
+        return View(itens);
+    }
+}
