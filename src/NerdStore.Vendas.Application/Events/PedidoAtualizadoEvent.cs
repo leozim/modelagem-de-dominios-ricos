5214e8659c39ee95f4c3b7e366363a1cd95bcaee
Leonardo Mariz Bezerra <leonardomariz@alu.ufc.br>
2026-08-12 17:57:54 -0300

    evento para atualização de pedidos

---
+++ src/NerdStore.Vendas.Application/Events/PedidoAtualizadoEvent.cs
@@ -0,0 +1,17 @@
+﻿using NerdStore.Core.Messages;
+
+namespace NerdStore.Vendas.Application.Events;
+
+public class PedidoAtualizadoEvent : Event
+{
+    public Guid ClienteId { get; private set; }
+    public Guid PedidoId { get; private set; }
+    public decimal ValorTotal { get; private set; }
+
+    protected PedidoAtualizadoEvent(Guid clienteId, Guid pedidoId, decimal valorTotal)
+    {
+        ClienteId = clienteId;
+        PedidoId = pedidoId;
+        ValorTotal = valorTotal;
+    }
+}
