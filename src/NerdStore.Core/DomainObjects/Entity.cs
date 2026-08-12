0035f978c7885af991d4cd8ff77508eb16f0aac0
Leonardo Mariz Bezerra <leonardomariz@alu.ufc.br>
2026-08-12 17:43:17 -0300

    adicinoando lista de evento a classe base Entity

--- src/NerdStore.Core/DomainObjects/Entity.cs
+++ src/NerdStore.Core/DomainObjects/Entity.cs
@@ -1,13 +1,34 @@
-﻿namespace NerdStore.Core.DomainObjects;
+﻿using NerdStore.Core.Messages;
+
+namespace NerdStore.Core.DomainObjects;
 
 public abstract class Entity
 {
+    public Guid Id { get; set; }
+    
+    private List<Event> _notificacoes;
+    public IReadOnlyCollection<Event> Notificacoes => _notificacoes?.AsReadOnly();
+    
     protected Entity()
     {
         Id = Guid.NewGuid();
     }
 
-    public Guid Id { get; set; }
+    public void AdicionarEvento(Event evento)
+    {
+        _notificacoes = _notificacoes ?? new List<Event>();
+        _notificacoes.Add(evento);
+    }
+
+    public void RemoverEvento(Event evento)
+    {
+        _notificacoes?.Remove(evento);
+    }
+
+    public void LimparEventos(Event eventItem)
+    {
+        _notificacoes?.Remove(eventItem);
+    }
 
     public override bool Equals(object obj)
     {
