828d26f8f52c61b5dcf54f8d3ab14f5cd0d2afc9
Leonardo Mariz Bezerra <leonardomariz@alu.ufc.br>
2026-08-12 20:30:32 -0300

    cart

--- src/NerdStore.WebApp.MVC/Extensions/CartViewComponent.cs
+++ src/NerdStore.WebApp.MVC/Extensions/CartViewComponent.cs
@@ -6,7 +6,8 @@ namespace NerdStore.WebApp.MVC.Extensions;
 public class CartViewComponent : ViewComponent
 {
     private readonly IPedidoQueries _pedidoQueries;
-
+    
+    // TODO: Obter cliente logado
     protected Guid ClienteId = Guid.Parse("244f74bf-158e-4778-b058-8372df7107a9");
 
     public CartViewComponent(IPedidoQueries pedidoQueries)
