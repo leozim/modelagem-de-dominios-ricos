5ac91224056ae2fa3b4da2db319a0a27e85a715b
Leonardo Mariz Bezerra <leonardomariz@alu.ufc.br>
2026-08-13 19:43:12 -0300

    implementado command do voucher

--- src/NerdStore.Vendas.Application/Commands/AplicarVoucherPedidoCommand.cs
+++ src/NerdStore.Vendas.Application/Commands/AplicarVoucherPedidoCommand.cs
@@ -5,7 +5,16 @@ namespace NerdStore.Vendas.Application.Commands;
 
 public class AplicarVoucherPedidoCommand : Command
 {
+    public Guid ClienteId { get; private set; }
+    public Guid PedidoId { get; private set; }
+    public string CodigoVoucher { get; private set; }
 
+    public AplicarVoucherPedidoCommand(Guid clienteId, Guid pedidoId, string codigoVoucher)
+    {
+        ClienteId = clienteId;
+        PedidoId = pedidoId;
+        CodigoVoucher = codigoVoucher;
+    }
 
     public override bool EhValido()
     {
@@ -18,6 +27,16 @@ public class AplicarVoucherPedidoValidation : AbstractValidator<AplicarVoucherPe
 {
     public AplicarVoucherPedidoValidation()
     {
+        RuleFor(c => c.ClienteId)
+            .NotEqual(Guid.Empty)
+            .WithMessage("Cliente Id invalido");
         
+        RuleFor(c => c.PedidoId)
+            .NotEqual(Guid.Empty)
+            .WithMessage("Pedido Id invalido");
+        
+        RuleFor(c => c.CodigoVoucher)
+            .NotEmpty()
+            .WithMessage("Codigo Voucher invalido ou nao pode ser vazio");
     }
 }
