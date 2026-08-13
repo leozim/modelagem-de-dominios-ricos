d153a2b313c12f24cb40795c98d33ac6e80ab53d
Leonardo Mariz Bezerra <leonardomariz@alu.ufc.br>
2026-08-13 19:39:36 -0300

    implementado command de remover item do pedido

--- src/NerdStore.Vendas.Application/Commands/RemoverItemPedidoCommand.cs
+++ src/NerdStore.Vendas.Application/Commands/RemoverItemPedidoCommand.cs
@@ -5,7 +5,16 @@ namespace NerdStore.Vendas.Application.Commands;
 
 public class RemoverItemPedidoCommand : Command
 {
+    public Guid ClienteId { get; private set; }
+    public Guid PedidoId { get; private set; }
+    public Guid ProdutoId { get; private set; }
 
+    public RemoverItemPedidoCommand(Guid clienteId, Guid pedidoId, Guid produtoId)
+    {
+        ClienteId = clienteId;
+        PedidoId = pedidoId;
+        ProdutoId = produtoId;
+    }
 
     public override bool EhValido()
     {
@@ -18,6 +27,16 @@ public class RemoverItemPedidoValidation : AbstractValidator<RemoverItemPedidoCo
 {
     public RemoverItemPedidoValidation()
     {
+        RuleFor(c => c.ClienteId)
+            .NotEqual(Guid.Empty)
+            .WithMessage("Cliente Id invalido");
         
+        RuleFor(c => c.PedidoId)
+            .NotEqual(Guid.Empty)
+            .WithMessage("Pedido Id invalido");
+        
+        RuleFor(c => c.ProdutoId)
+            .NotEqual(Guid.Empty)
+            .WithMessage("Produto Id invalido");
     }
 }
