cf5f6d93b4dfc0d254af65956aae5af2eeb997da
Leonardo Mariz Bezerra <leonardomariz@alu.ufc.br>
2026-08-13 21:12:36 -0300

    resolvendo dependency injection

--- src/NerdStore.WebApp.MVC/Setup/DependencyInjection.cs
+++ src/NerdStore.WebApp.MVC/Setup/DependencyInjection.cs
@@ -37,6 +37,11 @@ public static class DependencyInjection
         services.AddScoped<IRequestHandler<AdicionarItemPedidoCommand, bool>, PedidoCommandHandler>();
         services.AddScoped<IPedidoRepository, PedidoRepository>();
         services.AddScoped<IPedidoQueries, PedidoQueries>();
+        
+        services.AddScoped<IRequestHandler<AdicionarItemPedidoCommand, bool>, PedidoCommandHandler>();
+        services.AddScoped<IRequestHandler<AtualizarItemPedidoCommand, bool>, PedidoCommandHandler>();
+        services.AddScoped<IRequestHandler<RemoverItemPedidoCommand, bool>, PedidoCommandHandler>();
+        services.AddScoped<IRequestHandler<AplicarVoucherPedidoCommand, bool>, PedidoCommandHandler>();
 
         services.AddScoped<INotificationHandler<PedidoRascunhoIniciadoEvent>, PedidoEventHandler>();
         services.AddScoped<INotificationHandler<PedidoAtualizadoEvent>, PedidoEventHandler>();
