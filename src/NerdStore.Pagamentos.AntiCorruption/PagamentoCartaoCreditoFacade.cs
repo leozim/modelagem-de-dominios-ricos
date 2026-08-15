2985a0e914469e706328619a9f95c974c104075e
Leonardo Mariz Bezerra <leonardomariz@alu.ufc.br>
2026-08-15 19:29:36 -0300

    implementado facade de pagamaento cartao de credito

--- src/NerdStore.Pagamentos.AntiCorruption/PagamentoCartaoCreditoFacade.cs
+++ src/NerdStore.Pagamentos.AntiCorruption/PagamentoCartaoCreditoFacade.cs
@@ -1,6 +1,44 @@
-﻿namespace NerdStore.Pagamentos.AntiCorruption;
+﻿using NerdStore.Pagamentos.Business;
 
-public class PagamentoCartaoCreditoFacade
+namespace NerdStore.Pagamentos.AntiCorruption;
+
+public class PagamentoCartaoCreditoFacade : IPagamentoCartaoCreditoFacade
 {
-    
+    private readonly IPayPalGateway _payPalGateway;
+    private readonly IConfigurationManager _configurationManager;
+
+    public PagamentoCartaoCreditoFacade(IPayPalGateway payPalGateway, IConfigurationManager configurationManager)
+    {
+        _payPalGateway = payPalGateway;
+        _configurationManager = configurationManager;
+    }
+
+    public Transacao RealizarPagamento(Pedido pedido, Pagamento pagamento)
+    {
+        var apiKey = _configurationManager.Getvalue("apiKey");
+        var encriptionKey = _configurationManager.Getvalue("encriptionKey");
+
+        var serviceKey = _payPalGateway.GetPayPalServiceKey(apiKey, encriptionKey);
+        var cardHashKey = _payPalGateway.GetCardHashKey(serviceKey, pagamento.NumeroCartaao);
+
+        var pagamentoResult = _payPalGateway.CommitTransaction(cardHashKey, pedido.Id.ToString(), pagamento.Valor);
+        
+        // TODO: O gateway de pagamentos que deve retornar o obeto transação
+        var transacao = new Transacao
+        {
+            PedidoId = pedido.Id,
+            Total = pedido.Valor,
+            PagamentoId = pagamento.Id
+        };
+
+        if (pagamentoResult)
+        {
+            transacao.StatusTransacao = StatusTransacao.Pago;
+            return transacao;
+        }
+
+        transacao.StatusTransacao = StatusTransacao.Recusado;
+        return transacao;
+
+    }
 }
