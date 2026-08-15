4c6acc9faff3244309e4b27883589e3131a55d00
Leonardo Mariz Bezerra <leonardomariz@alu.ufc.br>
2026-08-15 19:15:04 -0300

    interface do paypal

--- src/NerdStore.Pagamentos.AntiCorruption/IPayPalGateway.cs
+++ src/NerdStore.Pagamentos.AntiCorruption/IPayPalGateway.cs
@@ -1,6 +1,8 @@
 ﻿namespace NerdStore.Pagamentos.AntiCorruption;
 
-public class IPayPalGateway
+public interface IPayPalGateway
 {
-    
+    string GetPayPalServiceKey(string apiKey, string encriptionKey);
+    string GetCardHashKey(string serviceKey, string cartaoCredito);
+    bool CommitTransaction(string cardhashKey, string orderId, decimal amount);
 }
