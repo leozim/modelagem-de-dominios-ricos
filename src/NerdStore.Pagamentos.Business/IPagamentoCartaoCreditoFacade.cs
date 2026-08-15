85cbb0a8dd9ffd5f176ae012b47340f2b16c62da
Leonardo Mariz Bezerra <leonardomariz@alu.ufc.br>
2026-08-15 18:38:10 -0300

    fazendo esoço da façade de apgamento. codigo não esta compilando no momento

---
+++ src/NerdStore.Pagamentos.Business/IPagamentoCartaoCreditoFacade.cs
@@ -0,0 +1,6 @@
+﻿namespace NerdStore.Pagamentos.Business;
+
+public interface IPagamentoCartaoCreditoFacade
+{
+    public Transacao RealizarPagamento(Pedido pedido, Pagamento pagamento);
+}
---
+++ src/NerdStore.Pagamentos.Business/Pagamento.cs
@@ -0,0 +1,18 @@
+﻿using NerdStore.Core.DomainObjects;
+
+namespace NerdStore.Pagamentos.Business;
+
+public class Pagamento : Entity, IAgregateRoot
+{
+    public Guid PedidoId { get; set; }
+    public string Status { get; set; }
+    public decimal Valor { get; set; }
+    
+    public string NomeCartao { get; set; }
+    public string NumeroCartaao { get; set; }
+    public string ExpiracaoCartao { get; set; }
+    public string CvvCartao { get; set; }
+    
+    // EF. Rel.
+    public Transacao Transacao { get; set; }
+}
