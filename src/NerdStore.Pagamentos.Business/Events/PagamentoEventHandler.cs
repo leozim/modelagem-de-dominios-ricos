be2c55ecb2af1f22dc102f609fc3561c9952e45b
Leonardo Mariz Bezerra <leonardomariz@alu.ufc.br>
2026-08-16 18:18:00 -0300

    implementando pagamento event handler e evento de integração de pedido estoque confirmado event

---
+++ src/NerdStore.Pagamentos.Business/Events/PagamentoEventHandler.cs
@@ -0,0 +1,31 @@
+﻿using MediatR;
+using NerdStore.Core.DomainObjects.DTO;
+using NerdStore.Core.Messages.CommonMessages.IntegrationEvents;
+
+namespace NerdStore.Pagamentos.Business.Events;
+
+public class PagamentoEventHandler : INotificationHandler<PedidoEstoqueConfirmadoEvent>
+{
+    private readonly IPagamentoService _pagamentoService;
+
+    public PagamentoEventHandler(IPagamentoService pagamentoService)
+    {
+        _pagamentoService = pagamentoService;
+    }
+
+    public async Task Handle(PedidoEstoqueConfirmadoEvent mensagem, CancellationToken cancellationToken)
+    {
+        var pagamentoPedido = new PagamentoPedidoDTO
+        {
+            PedidoId = mensagem.PedidoId,
+            ClienteId = mensagem.ClienteId,
+            Total = mensagem.Total,
+            NomeCartao = mensagem.NomeCartao,
+            NumeroCartao = mensagem.NumeroCartao,
+            ExpiracaoCartao = mensagem.ExpiracaoCartao,
+            CvvCartao = mensagem.CvvCartao
+        };
+        
+        await _pagamentoService.RealizarPagamentoPedido(pagamentoPedido);
+    }
+}
